ade06b841c8568cc6c2e5addd0c50eef00764f20
bvs3040 <taga-si@mail.ru>
2022-04-24 13:12:21 +0700

    task-2

--- task_2.js
+++ task_2.js
@@ -1,20 +1,15 @@
-function fun(obj){
-    for (let key in obj) {
-        if (obj.hasOwnProperty(key)) {
-            console.log(obj[key]); 
-        }   
-    }    
-}
+function fun(name, obj){
+    return thereIsProperty = name in obj;
+}    
 
-
-
+let thereIsProperty;
 const testPro = {
     d:"protoProperty"
 }
 const any = Object.create(testPro);
 any.a = 8;
 any.b = "string";
-any.c = 9;
+any.c = true;
 
-fun(any);
-console.log(any)
+fun('d', any);
+console.log(thereIsProperty)
